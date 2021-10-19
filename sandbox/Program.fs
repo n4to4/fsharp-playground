f77a5d9bdf8acf10c0d017cb2566bcc508bc8d8c
n4to4 <n4to4k@gmail.com>
2021-10-20 06:15:47 +0900

    init

---
+++ sandbox/Program.fs
@@ -0,0 +1,11 @@
+﻿// What is F#
+
+open System
+
+let names = [ "Peter"; "Julia"; "Xi" ]
+
+let getGreeting name = $"Hello, {name}"
+
+names
+|> List.map getGreeting
+|> List.iter (fun greeting -> printfn $"{greeting}! Enjoy your F#")
